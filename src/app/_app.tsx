48e1ee0d7f5e2169ab9c07d469eebb3cb3a73dce
Silas Afra <silasafra@gmail.com>
2023-06-03 11:13:23 -0300

    fix build

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
